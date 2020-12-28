9505a6b78bcf4f0e1826116d1da82a5801c8347a
Wells <brett.wells@leth.ca>
2020-12-28 13:19:22 -0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css';
+//import components
+import Form from './components/Form.js';
 
 function App() {
   return (
     <div className="App">
+    <header>
       <h1>Bretts To-do List</h1>
+    </header>
+    <Form />
     </div>
   );
 }
